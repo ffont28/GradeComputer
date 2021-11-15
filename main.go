4751cb88a14296237a3309f42aab755141244878
Francesco Fontana <72185312+ffont28@users.noreply.github.com>
2021-11-15 17:17:58 +0100

    Update main.go fixing a layout problem

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 import "fmt"
 func main() {
   var max int
+  fmt.Println("-------------")
+  fmt.Println("No copyright claimed by the author ffont28. For reports and/or suggestions contact on telegram @ffontana")
+  fmt.Println("----")
   fmt.Println("Benvenuto nel programma di calcolo di voto in proporzione.")
   fmt.Println("Per uscire clicca sulla X qui sopra nel terminale.")
   fmt.Print("Inserisci il massimo del punteggio che corrisponde al 10: ")
@@ -13,6 +16,6 @@ func main() {
     var votoFinale float64 = (float64(punteggio)*10.0)/float64(max)
         fmt.Printf("VOTO: %.2f   ||   VOTO CON CRITERIO ITIS: %.2f ", votoFinale, votoFinale-0.5);
     fmt.Println()
-        fmt.Println("---------------------------------------------\n");
+        fmt.Println("-------------------------------------------------");
   }
 }
